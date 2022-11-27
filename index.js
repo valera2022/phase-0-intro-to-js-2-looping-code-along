745a05e888d1b800624a302f9d873d675b837824
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-27 08:58:44 -0500

    commit tree 633c3c84038226a788a8db71ac2e84d5d15d3b8e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+ 
+function writeCards(strin,even){
+    let newA = []
+    for(let i = 0; i< strin.length; i++){
+        const message = `Thank you, ${strin[i]}, for the wonderful ${even} gift!`
+         newA.push(message)
+
+    }
+    return newA
+    
+    
+}
+
+
+// function countDown(integer){
+//     for(let i = 0; i>=integer; i-- ) { //use ; in the ()of the for loop
+//     console.log(`this is ${integer[i]} `)}
+
+// }
+
+
+function countDown(integers){
+{ while(integers>integers.length){
+        console.log(`this is ${integers}`)
+        integer--;}
+        }
+        console.log(integers)
+}
